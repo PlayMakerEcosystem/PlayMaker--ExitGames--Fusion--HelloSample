733e5b5828ac4148f65779fc75427c7d2cd9d43c
Jean Fabre <contact@fabrejean.net>
2021-06-30 14:49:58 +0300

    Added sirenix ( optional), brought back old sample code necessary for playmaker integration, added playmaker utils and playmaker fusion submodule

---
+++ Assets/PlayMaker Fusion Hello Sample/PlayMaker/Actions/Input/SetPlayerInput.cs
@@ -0,0 +1,89 @@
+﻿
+using HutongGames.PlayMaker.Addons.Fusion.Actions;
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Addons.Fusion.Sample
+{
+    [ActionCategory("Fusion Sample")]
+    [Tooltip("Sets the player Network Inputs")]
+    public class SetPlayerInput : FusionComponentActionBase<PlayMakerFusionInputDataProxy>
+    {
+        [CheckForComponent(typeof(PlayMakerFusionInputDataProxy))]
+        [Tooltip("The Game Object with the NetworkObject attached.")]
+        public FsmOwnerDefault gameObject;
+
+        public FsmBool searchInParent;
+        
+        [Tooltip("Respawn Input")]
+        public FsmBool respawn;
+        
+        [Tooltip("Boost Input")]
+        public FsmBool boost;
+        
+        [Tooltip("Left Input")]
+        public FsmBool left;
+        
+        [Tooltip("Right Input")]
+        public FsmBool right;
+        
+        [Tooltip("Bakward Input")]
+        public FsmBool backward;
+        
+        [Tooltip("Forward Input")]
+        public FsmBool forward;
+        
+        [Tooltip("Send this event if there was no PlayMakerFusionInputDataProxy Object found")]
+        public FsmEvent failure;
+        
+        public bool everyFrame;
+        
+        public override void Reset()
+        {
+            gameObject = null;
+            searchInParent = true;
+            
+            respawn = null;
+            boost = null;
+            left = null;
+            right = null;
+            backward = null;
+            forward = null;
+            
+            failure = null;
+            everyFrame = false;
+        }
+		
+        public override void OnEnter()
+        {
+            ExecuteAction();
+            if (!everyFrame)
+            {
+                Finish();
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            ExecuteAction();
+        }
+
+        void ExecuteAction()
+        {
+            if (!UpdateCache(Fsm.GetOwnerDefaultTarget(gameObject),searchParent: searchInParent.Value))
+            {
+                if (failure != null) Fsm.Event(failure);
+                return;	
+            }
+
+            
+            this.cachedComponent.CurrentInputData.Respawn = respawn.Value;
+            this.cachedComponent.CurrentInputData.Boost = boost.Value;
+            
+            this.cachedComponent.CurrentInputData.Left = left.Value;
+            this.cachedComponent.CurrentInputData.Right = right.Value;
+            this.cachedComponent.CurrentInputData.Backward = backward.Value;
+            this.cachedComponent.CurrentInputData.Forward = forward.Value;
+            
+        }
+    }
+}
---
+++ Assets/PlayMaker Fusion Hello Sample/Scripts/PlayMakerInputData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Fusion;
+
+namespace HutongGames.PlayMaker.Addons.Fusion.Sample
+{
+    /// <summary>
+    /// TODO: this will have to be generated, possibly from a dedicated fsm where we take the inputs
+    /// </summary>
+    [Serializable]
+    public struct PlayMakerInputData : INetworkInput
+    {
+        public NetworkBool Respawn;
+        public NetworkBool Boost;
+        
+        
+        public NetworkBool Forward;
+        public NetworkBool Backward;
+        public NetworkBool Left;
+        public NetworkBool Right;
+
+        /// <summary>
+        /// TODO: find a way to make this generic or easy to define
+        /// </summary>
+        public void resetStates()
+        {
+            Respawn = false;
+            Boost = false;
+
+            Forward = false;
+            Backward = false;
+            Left = false;
+            Right = false;
+        }
+    }
+}
